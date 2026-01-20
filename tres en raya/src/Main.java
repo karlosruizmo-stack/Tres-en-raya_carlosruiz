3a61f6ac0aece46b6ae6a7f2621f7335815a068c
karlosruizmo-stack <karlosruizmo@gmail.com>
2026-01-20 12:51:29 +0100

    Update Main.java

--- tres en raya/src/Main.java
+++ tres en raya/src/Main.java
@@ -21,7 +21,7 @@ public class TresEnRalla {
                 System.out.println("Jugador " + currentPlayer + "ha ganado");
                 break;
             }
-            if (isBoardFull()) {//Representacio visual del tablero
+            if (is BoardFull()) {//Representacio visual del tablero
                 printBoard();
                 System.out.println("Empate");
                 break;
@@ -37,6 +37,47 @@ public class TresEnRalla {
                     System.out.println("-------------");
                 }
                 }
+            private static void playerMove(){
+                Scanner in = new Scanner(System.in);
+                int row, col;
+
+                while(true){
+                    System.out.println("Jugador 1 esta eligiendo" + currentPlayer);
+                    row = scanner.nextInt();
+                    col = scanner.nextInt();
+
+                    if(row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col]
+                    == ' ') {
+                        board[row][col] = currentPlayer
+                    }else {
+                        System.out.println("No se puede hacer");
+                    }
+                }
+            }
+            private static boolean isWinner() {
+                for (int i = 0; i < 3; i++) {
+                    if ((board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) ||
+                            (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer)) {
+                        return true;
+                    }
+                }
+                f ((board[0][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) ||
+                        (board[0][2] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer)) {
+                    return true;
+                    rpivate static boolean isBoardFull() {
+                        for (int i = 0; i < 3; i++) {
+                        for (int j = 0; j < 3; j++) {
+                        if(board[i][j] == ' ') {
+                            return false;
+                        }
+                        }
+                    }
+                        return true;
+                    }
+                    private static void switchPlayer() {
+                        currentPlayer = (currentPlayer ==)
+                    }
+            }
                 }
 
             }
